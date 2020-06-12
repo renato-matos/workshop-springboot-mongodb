4d2906cdf7c06afb1a6eb4320e047f26a4eccef0
Renato Matos <renato.matos@creditas.comb.br>
2020-06-12 16:15:14 -0300

    Obtendo um usuário por id

--- src/main/java/com/galtconsulting/workshopmongo/services/UserService.java
+++ src/main/java/com/galtconsulting/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.galtconsulting.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.galtconsulting.workshopmongo.domain.User;
 import com.galtconsulting.workshopmongo.repository.UserRepository;
+import com.galtconsulting.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }

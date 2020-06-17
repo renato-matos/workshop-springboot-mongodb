0b42997e3df445349d26aa2f5a333c9fa1abc4b7
Renato Matos <renato.matos@creditas.comb.br>
2020-06-17 14:38:48 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/galtconsulting/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.galtconsulting.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.galtconsulting.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}

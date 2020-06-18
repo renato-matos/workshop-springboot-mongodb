9038943d7105e4e592d753be43e9a8a4eb2deb97
Renato Matos <renato.matos@creditas.comb.br>
2020-06-18 07:54:33 -0300

    Obtendo um post por id

---
+++ src/main/java/com/galtconsulting/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.galtconsulting.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.galtconsulting.workshopmongo.domain.Post;
+import com.galtconsulting.workshopmongo.repository.PostRepository;
+import com.galtconsulting.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
